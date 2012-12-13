a5f7b48bc1c40ea361541d77ef94303609c6c53a
hedayatyazdani <call@jointab.com>
2012-12-13 11:44:52 +0330

    Update signal.c
    Signals are event that happened from devices such as cameras , touch screen , water sensors , battery and ...
    
    Signals have type , target app 
    then types are defined in Signal Enumerate and target is app process id bigger than 0, if target was 0 then all of applications will receive the signal and if target was -1 the signal will send to the Operating system kernel

--- signal.c
+++ signal.c
@@ -14,15 +14,51 @@ enum signals
 };
 
 signal signal_que[Max_Signals];
+int current_signal=0;
 
 void addSignal(signal arg)
 {
-  for(i=0;i<Max_Signals;i++)
+  for(int i=0;i<Max_Signals;i++)
   {
     if(signal_que[i]==NULL)
     {
       signal_que[i]=arg;
     }
+  }
+}
+
+bool pushSignal(signal arg)
+{
+  bool ret=false;
+  for(int i=0;i<Max_Signals;i++)
+  {
+    if(signal_que[i]==NULL)
+    {
+      signal_que[i]=arg;
+      ret=true;
+    }
+  }
+  return ret;
+}
+
+signal popSignal()
+{
+  if(current_signal<Max_Signals)
+  {
+    if(signal_que[current_signal]!=NULL)
+    {
+      current_signal++;
+      return signal_que[current_signal-1];
+    }
+    else
+    {
+      popSignal();
+    }
+  }
+  else
+  {
+    current_signal=0;
+    popSignal();
   }
 }
 
